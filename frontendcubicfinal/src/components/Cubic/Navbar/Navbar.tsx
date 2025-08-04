b86036b2557e03036ce42300ea43120c8f423b2a
Cipher0023 <dani.esparza@hotmail.com>
2025-08-04 16:07:22 -0300

    sincronizando, agora com páginas do site criadas

--- frontendcubicfinal/src/components/Cubic/Navbar/Navbar.tsx
+++ frontendcubicfinal/src/components/Cubic/Navbar/Navbar.tsx
@@ -17,8 +17,8 @@ export default function Navbar() {
       label: "Projetos",
     },
     {
-      url: "/serviços",
-      label: "Serviços",
+      url: "/servicos",
+      label: "Servicos",
     },
     {
       url: "/sobre",
